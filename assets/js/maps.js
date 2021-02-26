0d9097eb3aa7b931792ac13346f8f86245c6f2ff
nickassafkirk <nickassafkirk@gmail.com>
2021-02-26 23:53:09 +0000

    Add external js file

---
+++ assets/js/maps.js
@@ -0,0 +1,30 @@
+function initMap() {
+    let map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    let locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
+
+    let markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    // Add a marker clusterer to manage the markers.
+    new MarkerClusterer(map, markers, {
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
+}
