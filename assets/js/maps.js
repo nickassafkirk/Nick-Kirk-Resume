02047b02773c0cda540bb59f33eae593a19fc509
nickassafkirk <nickassafkirk@gmail.com>
2021-02-27 21:01:58 +0000

    Fix maps error

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,5 @@
 function initMap() {
-    let map = new google.maps.Map(document.getElementById("map"), {
+    var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
             lat: 46.619261,
@@ -7,24 +7,20 @@ function initMap() {
         }
     });
 
-    let labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    let locations = [
+    var locations = [
         { lat: 40.785091, lng: -73.968285 },
         { lat: 41.084045, lng: -73.874245 },
         { lat: 40.754932, lng: -73.984016 }
     ];
 
-    let markers = locations.map(function (location, i) {
+    var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
             label: labels[i % labels.length]
         });
     });
 
-    // Add a marker clusterer to manage the markers.
-    new MarkerClusterer(map, markers, {
-        imagePath:
-            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-    });
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
